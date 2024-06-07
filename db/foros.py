d1a512e7cf9afb172f3652a87fae1d60bcbc8036
Carlos Maliqueo <89724571+Sp1ike@users.noreply.github.com>
2024-06-07 01:46:26 -0400

    Update foros.py

--- db/foros.py
+++ db/foros.py
@@ -15,3 +15,43 @@ def create_foro(id_comunidad, id_usuario, tipo_foro, estado_foro, tema_foro):
         return nuevo_foro
     finally:
         session.close()
+
+def get_foro(id_foro):
+    session = get_session()
+    try:
+        foro = session.query(Foro).filter(Foro.id_foro == id_foro).one()
+        return foro
+    finally:
+        session.close()
+
+def get_foros():
+    session = get_session()
+    try:
+        foros = session.query(Foro).all()
+        return foros
+    finally:
+        session.close()
+
+def delete_foro(id_foro):
+    session = get_session()
+    try:
+        foro = session.query(Foro).filter(Foro.id_foro == id_foro).one()
+        session.delete(foro)
+        session.commit()
+        return foro
+    finally:
+        session.close()
+
+def update_foro(id_foro, id_comunidad, id_usuario, tipo_foro, estado_foro, tema_foro):
+    session = get_session()
+    try:
+        foro = session.query(Foro).filter(Foro.id_foro == id_foro).one()
+        foro.id_comunidad = id_comunidad
+        foro.id_usuario = id_usuario
+        foro.tipo_foro = tipo_foro
+        foro.estado_foro = estado_foro
+        foro.tema_foro = tema_foro
+        session.commit()
+        return foro
+    finally:
+        session.close()
